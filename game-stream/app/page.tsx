1ddb71803f2f5d2b896c6e375a08d6bc40ab26ae
Raghav Bhandari <99389888+R-Bhandari@users.noreply.github.com>
2024-09-02 12:18:08 +0000

    Fix: some css for authentication module

--- game-stream/app/page.tsx
+++ game-stream/app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-   <Button>
-    click Me
-   </Button>
+   <div className = "flex flex-col gap-y-4">
+    <h1>Dashboard</h1>
+    <UserButton 
+      afterSignOutUrl = "/"
+    />
+   </div>
   );
 }
